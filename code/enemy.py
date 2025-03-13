fe6d7919276ca32ea3597638334c4539c0185124
Jardeson <jardeson.dev@gmail.com>
2025-03-12 21:38:32 -0300

    Adding classes StarUML

---
+++ code/enemy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Enemy:
+    def __init__(self):
+        self.image = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
+    def draw(self, ):
+        pass
